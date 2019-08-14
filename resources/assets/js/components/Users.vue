288dd726e07ac7e7d5fdd9bc28f0b3aa203f636f
Javad Jafariniya <javad1994ma@gmail.com>
2019-08-14 20:49:53 +0430

    part 10 seened and the modal add in the Users.vue Component

--- resources/assets/js/components/Users.vue
+++ resources/assets/js/components/Users.vue
@@ -1,12 +1,68 @@
 <template>
     <div class="container">
-        <div class="row">
-            <div class="card">
-                <div class="card-header">
-                    Users Header
+        <div class="row mt-3">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Users Table</h3>
+                        <div class="card-tools">
+                            <button data-toggle="modal" data-target="#addNew" class="btn btn-success">Add User
+                                <i class="fa fa-user-plus fa-fw"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="card-body table-responsive table-bordered">
+                        <table class="table table-hover">
+                            <tbody>
+                            <tr>
+                                <th>ID</th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Type</th>
+                                <th>Modify</th>
+                            </tr>
+                            <tr>
+                                <td>183</td>
+                                <td>John Doe</td>
+                                <td>11-7-2014</td>
+                                <td></td>
+                                <td>
+                                    <a href="#">
+                                        <i class="fa fa-user-edit"></i>
+                                    </a>
+                                    |
+                                    <a href="#">
+                                        <i class="fa fa-trash-alt red"></i>
+                                    </a>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <!-- /.box-body -->
                 </div>
-                <div class="card-body">
-                    Users Body
+                <!-- /.box -->
+            </div>
+        </div>
+        <!-- Modal -->
+        <div class="modal fade" id="addNew" tabindex="-1" role="dialog" aria-labelledby="addNewModal"
+             aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Add New</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Create</button>
+                    </div>
                 </div>
             </div>
         </div>
